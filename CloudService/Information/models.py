f1e112209df0c8dcac52040956930a81c62642b8
吕鑫 <lvxin14@163.com>
2016-10-05 12:42:00 +0800

    create profile tables, can get profile through API now

--- CloudService/Information/models.py
+++ CloudService/Information/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Profile(models.Model):
+    icon = models.CharField(max_length=200, blank=True, default='')
+    genders = models.IntegerField(default=0)
+    school = models.CharField(max_length=200, blank=True, default='')
+    department = models.CharField(max_length=200, blank=True, default='')
+    real_name = models.CharField(max_length=100, blank=True, default='')
+    owner = models.ForeignKey('auth.User', related_name='profile')
