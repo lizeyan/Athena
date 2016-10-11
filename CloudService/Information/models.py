e0813393c05859a79296aa70fd008fd92499e026
吕鑫 <lvxin14@163.com>
2016-10-12 01:14:08 +0800

    add new three models in information app, api to show them is to do

--- CloudService/Information/models.py
+++ CloudService/Information/models.py
@@ -1,6 +1,25 @@
 from django.db import models
-
+from account.models import Profile
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class ActivityGroup(models.Model):
+    group_id = models.CharField(max_length=64, blank=True)
+    activity_group_name = models.CharField(max_length=100, blank=True)
+    admin_user = models.ManyToManyField(Profile, related_name='activity_group_admin_set')
+    normal_user = models.ManyToManyField(Profile, related_name='activity_group_normal_set')
+
+
+class Activity(models.Model):
+    location = models.CharField(max_length=100, blank=True)
+    begin_time = models.DateField(blank=True)
+    end_time = models.DateField(blank=True)
+    activity_group = models.ForeignKey(ActivityGroup, related_name='activity')
+
+
+class RegisterLog(models.Model):
+    register_user = models.ForeignKey(Profile, related_name='register_log')
+    activity = models.ForeignKey(Activity, related_name='register_log')
 
