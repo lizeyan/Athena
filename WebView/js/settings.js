70ba7bce5d25f4e87757eece717496bd90ca443c
LI ZEYAN <zy-li14@hotmail.com>
2016-10-15 00:16:27 +0800

    patch files to server. Note that the patch is use multipart/form-data rather than application.json format.

--- WebView/js/settings.js
+++ WebView/js/settings.js
@@ -3,15 +3,15 @@
  */
 var PublicProfileView = Backbone.View.extend({
     el: $('#athena-profile-setting'),
+    imgFile: null,
     events: {
-        'submit #athena-public-profile-form': 'uploadProfile'
+        'submit #athena-public-profile-form': 'uploadProfile',
+        "change #athena-avatar-input": 'openAvatar'
     },
     template: _.template($('#tmplt-profile-setting').html()),
     render: function () {
-        alert(this.model.get('genders'));
         this.$el.html(this.template({
             "image": this.model.get('icon_image'),
-            "username": this.model.get('user').username,
             "realname": this.model.get('real_name'),
             "school": this.model.get('school'),
             "department": this.model.get('department'),
@@ -22,8 +22,41 @@ var PublicProfileView = Backbone.View.extend({
     initialize: function () {
         this.listenTo(this.model, 'change', this.render);
     },
-    uploadProfile: function () {
-
+    openAvatar: function (event) {
+        this.imgFile = event.currentTarget.files[0];
+        // alert (file);
+        var reader = new FileReader();
+        reader.onload = function (fileEvent) {
+            this.model.set({
+                icon_image: fileEvent.target.result
+            });
+        }.bind(this);
+        reader.onerror = function () {
+            console.log("error", arguments)
+        };
+        reader.readAsDataURL(this.imgFile);
+    },
+    uploadProfile: function (event) {
+        event.preventDefault();
+        var formData = new FormData;
+        formData.append('real_name', $('#athena-realname-input').val());
+        formData.append('icon_image', this.imgFile);
+        formData.append('school', $('#athena-school-input').val());
+        formData.append('department', $('#athena-department-input').val());
+        formData.append('genders', $('#athena-gender-input').val());
+        this.model.save(null, {
+            headers: {'Authorization': 'JWT ' + token},
+            error: function () {
+                alert('上传错误');
+                window.location.reload();
+            },
+            data: formData,
+            patch: true,
+            wait: true,
+            emulateJSON: true,
+            contentType: false,
+            processData: false
+        });
     }
 
 });
@@ -34,9 +67,10 @@ profile.fetch({
     headers: {'Authorization': 'JWT ' + token},
     success: function (model, response) {
         $('body').css('display', 'block');
-        document.title = model.get('user') + '(' + model.get('real_name') + ')';
+        document.title = model.get('user').username + '(' + model.get('real_name') + ')';
     }, error: function (model, response) {
         alert('加载错误！' + JSON.stringify(response));
         gobackLogin();
-    }
+    },
+    url: API_ROOT + '/profile/?format=json'
 });
--- WebView/js/user.js
+++ WebView/js/user.js
@@ -39,10 +39,11 @@ profile.fetch({
     headers: {'Authorization': 'JWT ' + token},
     success: function (model, response) {
         $('body').css('display', 'block');
-        document.title = model.get('user') + '(' + model.get('real_name') + ')';
+        document.title = model.get('user').username + '(' + model.get('real_name') + ')';
     }, error: function (model, response) {
         alert ('加载错误！' + JSON.stringify(response));
         gobackLogin();
-    }
+    },
+    url: API_ROOT + '/profile/?format=json'
 });
 
