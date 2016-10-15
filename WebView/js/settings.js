07d5709c235e5e86c5bf0c5a6ef7e233a21f9769
LI ZEYAN <zy-li14@hotmail.com>
2016-10-15 17:01:58 +0800

    Add the top navbar for signed-in pages. Add a button for logout and switch between pages

--- WebView/js/settings.js
+++ WebView/js/settings.js
@@ -64,9 +64,13 @@ profile.fetch({
     success: function (model, response) {
         $('body').css('display', 'block');
         document.title = model.get('user').username + '(' + model.get('real_name') + ')';
+
     }, error: function (model, response) {
         alert('加载错误！' + JSON.stringify(response));
         gobackLogin();
     },
     url: API_ROOT + '/profile/?format=json'
+});
+profile.on('change:icon_image', function () {
+    SetTopNavBar ({img: profile.get('icon_image')});
 });
