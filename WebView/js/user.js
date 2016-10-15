d0fdec815403b7601bd454966ed3187189d5bcab
LI ZEYAN <zy-li14@hotmail.com>
2016-10-15 15:24:07 +0800

    show admined and normal activity group in user page. And I add a router for it

--- WebView/js/user.js
+++ WebView/js/user.js
@@ -2,6 +2,7 @@
  * Created by zy-li14 on 16-10-9.
  */
 //现在得到了一个profile，需要渲染界面
+//左侧的名片和邮件的显示
 var NameCard = Backbone.View.extend ({
     el: $('#namecard'),
     initialize: function () {
@@ -31,10 +32,47 @@ var Email = Backbone.View.extend ({
         return this;
     }
 });
+//管理的所有活动的显示
+var ActivityGroupCard = Backbone.View.extend ({
+    tagName: 'div',
+    template: _.template($('#tmplt-activity-group').html()),
+    render: function (option) {
+        this.$el.html(this.template(option));
+        return this;
+    }
+});
+var AdminActivityGroupList = Backbone.View.extend ({
+    el: $('#athene-admin-activity-group-list'),
+    initialize: function () {
+        this.listenTo(this.model, 'change:admin_activity_group', this.render);
+    },
+    render: function () {
+        this.$el.empty();
+        _.each(this.model.get('admin_activity_group'), function (activity_group) {
+            this.$el.append((new ActivityGroupCard).render({'activity_group_name': activity_group.activity_group_name, 'url': activity_group.url}).$el);
+        }, this);
+        return this;
+    }
+});
+var NormalActivityGroupList = Backbone.View.extend ({
+    el: $('#athene-normal-activity-group-list'),
+    initialize: function () {
+        this.listenTo(this.model, 'change:normal_activity_group', this.render);
+    },
+    render: function () {
+        this.$el.empty();
+        _.each(this.model.get('normal_activity_group'), function (activity_group) {
+            this.$el.append((new ActivityGroupCard).render({'activity_group_name': activity_group.activity_group_name, 'url': activity_group.url}).$el);
+        }, this);
+        return this;
+    }
+});
 //定义完了View和Model，正常顺序处理
 var profile = new Profile;
 var namecard = new NameCard ({model: profile});
 var email = new Email ({model: profile});
+var adminActivityGroupList = new AdminActivityGroupList ({model: profile});
+var normalActivityGroupList = new NormalActivityGroupList ({model: profile});
 profile.fetch({
     headers: {'Authorization': 'JWT ' + token},
     success: function (model, response) {
@@ -47,3 +85,37 @@ profile.fetch({
     url: API_ROOT + '/profile/?format=json'
 });
 
+//my router
+$(function () {
+    var Router = Backbone.Router.extend({
+        routes: {
+            "overview": "showOverview",
+            "admin-activity-group": "showAdminActivityGroup",
+            "normal-activity-group": "showNormalActivityGroup",
+            "*path": "showOverview"
+        },
+        showOverview: function () {
+            this.deactivateAll();
+            $('#athena-main-nav-overview').addClass('active');
+        },
+        showAdminActivityGroup: function () {
+            this.deactivateAll();
+            adminActivityGroupList.$el.show();
+            $('#athena-main-nav-admin').addClass('active');
+        },
+        showNormalActivityGroup: function () {
+            this.deactivateAll();
+            normalActivityGroupList.$el.show();
+            $('#athena-main-nav-normal').addClass('active');
+        },
+        deactivateAll: function () {
+            _.each($('#athena-main-nav').children('li'), function (li) {
+                $(li).removeClass('active');
+            });
+            adminActivityGroupList.$el.hide();
+            normalActivityGroupList.$el.hide();
+        }
+    });
+    var router = new Router;
+    Backbone.history.start();
+});
